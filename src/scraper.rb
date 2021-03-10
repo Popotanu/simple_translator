e0601199d70a2adaf4a96383eaf21cbf56b789b7
popotanu <scrapmacaron@gmail.com>
2021-03-10 12:32:38 +0900

    add: scrape an articles list

--- src/scraper.rb
+++ src/scraper.rb
@@ -5,17 +5,25 @@ require "bundler/setup"
 require "mechanize"
 require "pry-byebug"
 
-TARGET_URI = URI("https://ro.gnjoy.com/news/devnote/list.asp")
-
 module Scraper
   class KRoOfficial
+    BASE_PATH = URI("https://ro.gnjoy.com")
+    LIST_PATH = URI("news/devnote/list.asp")
+    LIST_URI = BASE_PATH + LIST_PATH
+
     def initialize
-      @url = TARGET_URI
       @results = []
       @scraper = prepare_scraper
-      @landing_uri = URI("")
+      @landing_uri = LIST_URI
       @post_uris = ["https://ro.gnjoy.com/news/devnote/View.asp?category=1&seq=4096364&curpage=1"].map{ URI(_1) }
     end
+
+    def scrape
+      scrape_list
+      scrape_atricle
+    end
+
+    # private
 
     def prepare_scraper
       Mechanize.new { |agent|
@@ -23,11 +31,23 @@ module Scraper
       }
     end
 
-    def scrape
-
-    end
-
     def scrape_list
+      @scraper.get(@landing_uri) do |list_page|
+        posts = list_page.search(".devnote tbody tr")
+
+        posts.each do |post|
+          result = {}
+          detail_path = post.search("a").attribute("href").content
+          result[:uri] = BASE_PATH + URI(detail_path)
+
+          post.elements.each_with_object(result) do |e, res|
+            res[e.values.first.to_sym] = e.content.strip
+          end
+
+          @results << Scraper::Result.new(result)
+        end
+      end
+      @results
     end
 
     def scrape_article
@@ -36,36 +56,30 @@ module Scraper
       post = {}
 
       @scraper.get(post_page) do |page|
-
         published_date = page.search(".postDate").text.scan(/\d{4}\.\d{2}\.\d{2}/).first
         post[:published_date] = Date.parse(published_date).to_s
 
-        h1 = page.search('h1')
-        post[:category] = h1.search('.iconDev').text 
+        h1 = page.search("h1")
+        post[:category] = h1.search(".iconDev").text
 
         article = page.search("article .forPost")
-        post[:body] = article.inner_text 
-        post[:images] = article.search("img").map do 
+        post[:body] = article.inner_text
+        post[:images] = article.search("img").map do
           _1.attributes["src"].value
         end
       end
 
       @results << Scraper::Result.new(post)
     end
-
-    def scrape_title
-    end
-
-    def scrape_body
-    end
   end
 
   class Result
     include Comparable
 
-    attr_reader :title, :uri, :category, :body, :images, :published_date
+    attr_reader :num, :title, :uri, :category, :body, :images, :published_date
 
-    def initialize(article={})
+    def initialize(article = {})
+      @num = article[:num]
       @title = article[:title]
       @published_date = article[:published_date]
       @body = article[:body]
@@ -75,9 +89,9 @@ module Scraper
     end
 
     def <=>(other)
-      if @published_date < other.published_date
+      if @num < other.num
         -1
-      elsif @published_date == other.published_date
+      elsif @num == other.num
         0
       else
         1
@@ -87,17 +101,13 @@ module Scraper
     def ==(other)
       @title == other.title &&
         @published_date == other.published_date &&
-          @uri == other.uri
+        @uri == other.uri
     end
   end
 end
 
 # to avoid executing real-scraping while running tests.
 unless @testing
-  Scraper::KRoOfficial.new.scrape_article
+  pp Scraper::KRoOfficial.new.scrape_article if false
+  pp Scraper::KRoOfficial.new.scrape_list if false
 end
-
-
-
-
-
--- tests/test_scraper.rb
+++ tests/test_scraper.rb
@@ -1,23 +1,24 @@
 require 'test/unit'
 require_relative '../src/scraper'
 
-
-
 module Scraper
   class TestResult < Test::Unit::TestCase
     def setup
       @testing = true
-
       now = Date.parse('2020-01-02')
-      @me = Scraper::Result.new(published_date: now, title: 'tanu' )
+
+      num = 2
+      @me = Scraper::Result.new(num: num,published_date: now, title: 'tanu' )
+
+      @latest = Scraper::Result.new(num: num.succ)
+      @oldest = Scraper::Result.new(num: num.pred)
 
       @similar = Scraper::Result.new(published_date: now, title: 'kitune' )
 
-      @latest = Scraper::Result.new(published_date: Date.parse('2020-01-03'))
-      @oldest = Scraper::Result.new(published_date: Date.parse('2020-01-01'))
     end
 
     def test_comparable
+
       assert_equal true, @me < @latest
       assert_equal false, @me < @me
 
@@ -29,8 +30,8 @@ module Scraper
     end
 
     def test_equality
+
       assert_equal true, @me == @me
-      assert_equal false, @me == @latest
       assert_equal false, @me == @similar
     end
   end
